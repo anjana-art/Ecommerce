ca528b34a64d62c37aab0bf136c7a34848b66303
anjana-art <68542773+anjana-art@users.noreply.github.com>
2024-08-12 08:57:53 +0100

    added profile page

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,17 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+async function registerUser(e){
+ e.preventDefault();
+ try{
+  await axios.post('/api/users/register', {
+    firstName,
+    lastName,
+    email,
+    password,
+   });
+
+   alert('Successful registration');
+ }catch(e){
+  alert('Failed Registration, Try again. used email.')
+ }
+
+}
+
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Register </h1>
-        <form className="max-w-md mx-auto ">
-          <input type="text" placeholder="firstname"></input>
-          <input type="text" placeholder="lastname"></input>
-          <input type="email" placeholder="your_email@email.com"></input>
-          <input type="password" placeholder="*****"></input>
-          <button className="primary">Login</button>
+        <form className="max-w-md mx-auto " onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="firstname"
+            value={firstName}
+            onChange={(ev) => setFirstName(ev.target.value)}
+          ></input>
+          <input
+            type="text"
+            placeholder="lastname"
+            value={lastName}
+            onChange={(ev) => setLastName(ev.target.value)}
+          ></input>
+          <input
+            type="email"
+            placeholder="your_email@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="*****"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          ></input>
+          <button className="primary">Register</button>
           <div className="text-center py-2 text-gray-500">
             Already have an Account?
             <Link className="underline text-black" to="/login">
